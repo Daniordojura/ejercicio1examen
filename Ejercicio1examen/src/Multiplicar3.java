7b4c8824fe7858032d77487fc2bc6561dfc77723
Daniel <Daniel@192.168.1.189>
2019-05-26 09:20:25 +0200

    Version 2 Se añade un tercer valor

--- Ejercicio1examen/src/Multiplicar3.java
+++ Ejercicio1examen/src/Multiplicar3.java
@@ -4,19 +4,21 @@
 	public class Multiplicar3 {
 		int num1;
 		int num2;
+		int num3;
 		
-		public Multiplicar3(int num1, int num2) {
+		public Multiplicar3(int num1, int num2, int num3) {
 			num1 = num1;
 			num2 = num2;
+			num3 = num3;
 		}
 		
 		public int multiplicar() {
-			int resultado = num1 * num2;
+			int resultado = num1 * num2 * num3;
 			return resultado;
 		}
 
 	public static void main(String[] args) {
-		Multiplicar3 c1=new Multiplicar3(2,5);
+		Multiplicar3 c1=new Multiplicar3(2,5, 10);
 		System.out.println(c1.multiplicar());
 		
 	}
